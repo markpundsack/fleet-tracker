db50445655b631cad7c3622d70e5c98d44085d18
Mark Pundsack <mark@chiliranch.com>
2010-11-23 11:22:07 -0800

    trust cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,7 @@ module ApplicationHelper
     content_tag(:abbr, time.to_s, options.merge(:title => time.getutc.iso8601)) if time
   end
   
-  # def deliver_trust_message(trust_message = "This web site is requesting your trust.") 
-  #   trust_uri = "http://#{request.host}/" 
-  #   head 'eve.trustme' => "#{trust_uri}::#{trust_message}" 
-  # end
-  
-  def request_trust(trust_url = "http://#{request.host}"+((request.port)?":#{request.port}":""), *args)
+  def request_trust(trust_url = "http://#{request.host}"+((request.port)?":#{request.port}":"")+"/", *args)
     trust_url = url_for(trust_url.merge(:only_path => false)) if trust_url.kind_of?(Hash)
     javascript_tag "CCPEVE.requestTrust(#{trust_url.inspect});", *args
   end
