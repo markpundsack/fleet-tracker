bb2f946cc032f27ba1745cc3ab87438888beb37e
Mark Pundsack <mark@chiliranch.com>
2011-01-14 14:00:24 -0800

    pluralized neuts and reds

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,8 +1,8 @@
 module ReportsHelper
   def report_in_words(r)
     out = "#{r.char_name} in #{r.solar_system_name}"
-    out += ", #{r.reds} reds" unless (r.reds.nil? || r.reds==0)
-    out += ", #{r.neutrals} neuts" unless (r.neutrals.nil? || r.neutrals==0)
+    out += ", " + pluralize(r.reds,"red") unless (r.reds.nil? || r.reds==0)
+    out += ", " + pluralize(r.neutrals, "neut") unless (r.neutrals.nil? || r.neutrals==0)
     out += ", clear" if r.clear?
     #out += "#{time_ago_in_words(r.updated_at)} ago"
     return out
